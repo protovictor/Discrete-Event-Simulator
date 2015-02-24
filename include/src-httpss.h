8e9b17239beb0f3347091066a5e0b6fb94d2e8ed
Niang El hadji <eniang@dazzler.enseeiht.fr>
2015-02-24 16:23:46 +0100

    include/src-httpss.h

--- include/src-httpss.h
+++ include/src-httpss.h
@@ -43,42 +43,42 @@ struct srcHTTPSS * srcHTTPSS_init(struct randomGenerator_t * Sm,struct randomGen
  * @param Sm Type of Random, Generator Size of the main object in a page
  * @param src HTTP source
 */
-struct srcHTTPSS * srcHTTPSS_setSm(struct srcHTTPSS_t * src, struct randomGenerator_t * Sm)
+struct srcHTTPSS * srcHTTPSS_setSm(struct srcHTTPSS_t * src, struct randomGenerator_t * Sm);
 
 /**
  * @brief Set the Random Generator Se on the HTTP structure
  * @param Se Type of Random, Size of an embedded object in a page
  * @param src HTTP source
 */
-struct srcHTTPSS * srcHTTPSS_setSe(struct srcHTTPSS_t * src, struct randomGenerator_t * Se)
+struct srcHTTPSS * srcHTTPSS_setSe(struct srcHTTPSS_t * src, struct randomGenerator_t * Se);
 
 /**
  * @brief Set the Random Generator Nd on the HTTP structure
  * @param Nd Random Generator, Number of embedded objects in a page
  * @param src HTTP source
 */
-struct srcHTTPSS * srcHTTPSS_setNd(struct srcHTTPSS_t * src, struct randomGenerator_t * Nd)
+struct srcHTTPSS * srcHTTPSS_setNd(struct srcHTTPSS_t * src, struct randomGenerator_t * Nd);
 
 /**
  * @brief Set the Random Generator Dpc on the HTTP structure
  * @param  Dpc Random Generator, Reading time
  * @param src HTTP source
 */
-struct srcHTTPSS * srcHTTPSS_setDpc(struct srcHTTPSS_t * src, struct randomGenerator_t * Dpc)
+struct srcHTTPSS * srcHTTPSS_setDpc(struct srcHTTPSS_t * src, struct randomGenerator_t * Dpc);
 
 /**
  * @brief Set the Random Generator Tp on the HTTP structure
  * @param  Tp Random Generator, Parsing time for the main page.
  * @param src HTTP source
 */
-struct srcHTTPSS * srcHTTPSS_setTp(struct srcHTTPSS_t * src, struct randomGenerator_t * Tp)
+struct srcHTTPSS * srcHTTPSS_setTp(struct srcHTTPSS_t * src, struct randomGenerator_t * Tp);
 
 /**
  * @brief Set the Random Generator MTU on the HTTP structure
  * @param  MTU is the maximum transmission unit of the link
  * @param src HTTP source
 */
-struct srcHTTPSS * srcHTTPSS_setMTU(struct srcHTTPSS_t * src, int MTU)
+struct srcHTTPSS * srcHTTPSS_setMTU(struct srcHTTPSS_t * src, int MTU);
 
 /**
  * @brief Set the Random Generator MTU on the HTTP structure
@@ -86,7 +86,7 @@ struct srcHTTPSS * srcHTTPSS_setMTU(struct srcHTTPSS_t * src, int MTU)
  * @param nbTCP nombre de connections TCP utilisé pour charger les objets (==1 en 1.1)
  * @param src HTTP source
 */
-struct srcHTTPSS * srcHTTPSS_setversion(struct srcHTTPSS_t * src, int version, int nbTCP)
+struct srcHTTPSS * srcHTTPSS_setversion(struct srcHTTPSS_t * src, int version, int nbTCP);
 
 /*------------------------------------------------------------------------------------------*/
 			/*Fonctions pour lancer la session HTTP*/
@@ -105,9 +105,9 @@ void srcHTTPSS_sessionStart(struct srcHTTPSS * src, void * destination, processP
 							double RTTmd, int initialWindow);
 
 /**
- * @brief Dans cette fonction on va créer un source TCP (d'ou les paramètres pour la connection TCP).
- * On va envoyer la page principale à partir de cette fonction, et programmer le chargement
- * des objets embarqués
+ * @brief Dans cette fonction on va créer des sources TCP (d'ou les paramètres pour connections TCP).
+ * On va envoyer les objets embarquées à partir de cette fonction, et programmer le chargement
+ * d'une nouvelle page. Le chargement de la nouvelle page est traitée par srcHTTPSS_loadNewPage
  * @param src source HTTP, we have src-> MTU as maximum transmission unit of the link
  * @param RTTmd is the Round Trip Time minus transmission time on the access link
  * @param initialWindow is the initial value of cwnd
@@ -115,7 +115,20 @@ void srcHTTPSS_sessionStart(struct srcHTTPSS * src, void * destination, processP
  * @param destProcessPDU is the PDU processing function of the destination
 */
 void srcHTTPSS_sendEmbeddedObjects(struct srcHTTPSS * src, void * destination,
-									processPDU_t destProcessPDU, double RTTmd,
-									int initialWindow);
-void srcHTTPSS_loadNewPage(src, void * destination, processPDU_t destProcessPDU, 
-									double RTTmd, int initialWindow);
+					processPDU_t destProcessPDU, double RTTmd,int initialWindow);
+
+/**
+ * @brief Dans cette fonction on va créer une source TCP (d'ou les paramètres pour la connection TCP).
+ * On va envoyer vas pouvoir charger la nouvelle page
+ * à partir de cette fonction, et programmer le chargement des nouveaux objets embarqués.
+ * Le chargement des objets embarquées est traitée par srcHTTPSS_sendEmbeddedObjects
+ * @param src source HTTP, we have src-> MTU as maximum transmission unit of the link
+ * @param RTTmd is the Round Trip Time minus transmission time on the access link
+ * @param initialWindow is the initial value of cwnd
+ * @param destination is a pointer to the destination entity
+ * @param destProcessPDU is the PDU processing function of the destination
+*/
+void srcHTTPSS_loadNewPage(src, void * destination, processPDU_t destProcessPDU,
+			double RTTmd, int initialWindow);
+
+#endif
